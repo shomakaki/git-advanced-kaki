594d33ad42f90dfc78e6a137b7190a59177d2a2b
shomakaki <shoma.kaki@rakus-partners.co.jp>
2021-11-18 10:31:05 +0900

    first commit

---
+++ src/main/java/com/example/demo/domain/Car.java
@@ -0,0 +1,32 @@
+package com.example.demo.domain;
+
+public class Car {
+	private int speed;
+	private String name;
+	
+	public Car() {};
+	
+	public Car(int speed, String name) {
+		super();
+		this.speed = speed;
+		this.name = name;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Car [speed=" + speed + ", name=" + name + "]";
+	}
+	
+	
+}
